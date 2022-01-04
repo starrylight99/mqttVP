a18ef89eb56c3462d7a95577301b2806fc110490
starrylight99 <e0564873@u.nus.edu>
2022-01-04 11:31:25 +0800

    first commit

---
+++ aws.py
@@ -0,0 +1,187 @@
+from tkinter import Event
+import boto3
+import json
+import os
+import paho.mqtt.client as mqtt
+from pympler.tracker import SummaryTracker
+import threading
+import shutil
+import subprocess
+import sys
+import signal
+import time
+import tracemalloc
+
+_isLinux   = sys.platform.startswith('linux')
+_isWindows = sys.platform.startswith('win')
+script_dir = os.path.dirname(__file__)
+
+tracker = SummaryTracker()
+tracemalloc.start()
+scheduleEvent = threading.Event()
+isPlaying = threading.Event()
+
+with open('config.json', 'r') as f:
+    data = json.load(f)
+
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id = data["accessKeyId"], 
+    aws_secret_access_key = data["secretAccessKey"], 
+    region_name = data["region"]
+)
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id = data["accessKeyId"], 
+    aws_secret_access_key = data["secretAccessKey"], 
+    region_name = data["region"]
+)
+
+def memoryProfiler():
+    while True:
+        snapshot = tracemalloc.take_snapshot()
+        top_stats = snapshot.statistics('lineno')
+        print("[ Top 10 ]")
+        for stat in top_stats[:10]:
+            print(stat)
+        tracker.print_diff()
+        print(time.asctime(time.localtime(time.time())))
+        time.sleep(1800)
+
+def deleteSchedule(schedule, client):
+    path = 'downloads/schedules/' + schedule
+    shutil.rmtree(path, ignore_errors=True)
+    client.publish('webApp', 'deleted ' + data['id'])
+
+def ping_listdir(client):
+    path = 'downloads/schedules/'
+    if os.path.exists(path):
+        schedules = os.listdir(path)
+        res = 'online ' + data['id']
+        for i in schedules:
+            statusFile = open(path + i + '/status.txt','r')
+            status = statusFile.readline()
+            if status == 'true':
+                client.publish('webApp', f"{res} {i}")
+    else:
+        client.publish('webApp', 'online ' + data['id'] + ' nil')
+        
+
+def downloadSchedule(group, schedule, client):
+    os.makedirs('downloads/schedules/' + schedule, exist_ok=True)
+    statusFile = open('downloads/schedules/' + schedule + '/status.txt', 'w')
+    statusFile.write('false')
+    statusFile.close()
+
+    sched_path = 'downloads/schedules/' + schedule + '/' + schedule + '_schedule.json'
+    s3.Bucket('maventest1').download_file(group + '/schedules/' + schedule + '/' + schedule + '_schedule.json', sched_path)
+    with open(sched_path, 'r') as f:
+        schedule_config = json.load(f)
+    for playlist in schedule_config['uniquePlaylist']:
+        response = s3_client.list_objects_v2(
+            Bucket='maventest1',
+            Delimiter='/',
+            Prefix= group + '/schedules/' + schedule + '/' + playlist + '/'  
+        )
+        playlist_path = 'downloads/schedules/' + schedule + '/' + playlist
+        os.makedirs(playlist_path, exist_ok=True)
+        for key in response["Contents"]:
+            s3.Bucket('maventest1').download_file(key["Key"], playlist_path + '/' + key["Key"].split('/')[-1])
+    client.publish('webApp', 'Finished Downloading')
+
+    statusFile = open('downloads/schedules/' + schedule + '/status.txt', 'w')
+    statusFile.write('true')
+    statusFile.close()
+
+def startSchedule(scheduleName):
+    while not scheduleEvent.is_set():
+        rel_path = "startSchedule.pid"
+        abs_file_path = os.path.join(script_dir, rel_path)
+
+        if _isLinux:
+            p = subprocess.Popen(f'python3 vlcSchedule.py {scheduleName}', shell=True, preexec_fn=os.setsid)
+        elif _isWindows:
+            p = subprocess.Popen(f'python vlcSchedule.py {scheduleName}', shell=True, preexec_fn=os.setsid)
+
+        output_pid = open(abs_file_path, "w")
+        output_pid.write(str(os.getpgid(p.pid)))
+        output_pid.close()
+        p.wait()
+    scheduleEvent.clear()
+
+        #if p.poll() is not None:
+        #    os.kill(p.pid, signal.SIGTERM)
+
+def on_disconnect(client, userdata, rc=0):
+    print("Disconnected result code " + str(rc))
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected flags" + str(flags) + "result code " + str(rc))
+
+def on_publish(client, userdata, mid):
+    print("message published " + str(mid))
+
+def on_log(client, userdata, level, buf):
+    print("log: ",buf)
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print('subscribed schedule')
+
+def on_message(client, userdata, message):
+    msg = message.payload.decode()
+    split_msg = msg.split(' ')
+    print(split_msg)
+    if msg == "ping":
+        pingThread = threading.Thread(target=ping_listdir, args=(client,))
+        pingThread.start()
+    elif split_msg[1] == data['id']:
+        if split_msg[0] == 'schedule':
+            if len(split_msg) > 3:
+                msg = " ".join(split_msg[2:])
+                downloadThread = threading.Thread(target=downloadSchedule, args=(data['group'], msg, client))
+            else:
+                downloadThread = threading.Thread(target=downloadSchedule, args=(data['group'], split_msg[2], client))
+            downloadThread.start()
+        elif split_msg[0] == 'delete':
+            schedule = " ".join(split_msg[2:])
+            deleteThread = threading.Thread(target=deleteSchedule, args=(schedule, client))
+            deleteThread.start()
+        elif split_msg[0] == 'run':
+            if not isPlaying.is_set():
+                schedule = " ".join(split_msg[2:]) if len(split_msg) > 2 else split_msg[2]
+                client.publish('webApp', f'running {schedule}' )
+                isPlaying.set()
+                scheduleThread = threading.Thread(target=startSchedule, args=(schedule,))
+                scheduleThread.start()
+            else:
+                client.publish('webApp', 'already running, cant run')
+        elif split_msg[0] == 'stop':
+            if isPlaying.is_set():
+                client.publish('webApp', 'stopped')
+                isPlaying.clear()
+                scheduleEvent.set()
+                pidF = open("startSchedule.pid", "r+")
+                pid = int(pidF.readline())
+                os.killpg(pid, signal.SIGTERM)
+                pidF.truncate(0)
+                pidF.close()
+            else:
+                client.publish('webApp', 'not running, cant stop')
+
+
+broker_address = '18.141.182.21'
+threading.Thread(target=memoryProfiler).start()
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_publish = on_publish
+client.on_log = on_log
+client.on_subscribe = on_subscribe
+client.on_message = on_message
+client.username_pw_set(username="maventest",password="12345")
+client.tls_set('ca1.crt')
+client.tls_insecure_set(True)
+client.connect(broker_address, 8883)
+client.subscribe('schedule')
+client.subscribe('ping')
+client.loop_forever()
---
+++ vlcSchedule.py
@@ -0,0 +1,262 @@
+import os
+import vlc
+import time
+import sys
+import tkinter as tk
+import threading as td
+import json
+from pympler.tracker import SummaryTracker
+from PIL import ImageTk, Image
+from screeninfo import get_monitors
+
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
+
+_isLinux   = sys.platform.startswith('linux')
+_isWindows = sys.platform.startswith('win')
+
+""" script_dir = os.path.dirname(__file__)
+
+rel_path = "vlcSchedule.pid"
+abs_file_path = os.path.join(script_dir, rel_path)
+
+output_pid = open(abs_file_path, "w")
+pid = str(os.getpid())
+output_pid.write(pid)
+output_pid.close() """
+
+wday = {
+    'Monday':0,
+    'Tuesday':1,
+    'Wednesday':2,
+    'Thursday':3,
+    'Friday':4,
+    'Saturday':5,
+    'Sunday':6
+}
+
+scheduleCount,scheduleDict = 1,{}
+directory = "downloads/schedules"
+scheduleName = " ".join(sys.argv[1:]) if len(sys.argv) > 2 else sys.argv[1]
+
+#scheduleName = 'useThis'
+configFile = open(f'{directory}/{scheduleName}/{scheduleName}_schedule.json')
+scheduleConfig = json.load(configFile)
+
+scheduleArray = []
+
+# init 3d array of 0
+for i in range(7):
+    scheduleArray.append([])
+    for j in range(24):
+        scheduleArray[i].append([])
+        for k in range(60):
+            scheduleArray[i][j].append(0)
+
+# set timings to number, then map to 3d array of schedule
+for timing in scheduleConfig['schedule']:
+    for dayStr in timing['days']:
+        day = wday[dayStr]
+        scheduleDict[scheduleCount] = timing['playlists']
+        startTime, endTime = timing['startTime'].split(':'),timing['endTime'].split(':')
+        if int(startTime[0]) != int(endTime[0]):
+            for minutes in range (int(startTime[1]),60):
+                scheduleArray[day][int(startTime[0])][minutes] = scheduleCount
+                
+            for minutes in range (0,int(endTime[1]) + 1):
+                scheduleArray[day][int(endTime[0])][minutes] = scheduleCount
+        else:
+            for minutes in range (int(startTime[1]),int(endTime[1]) + 1):
+                scheduleArray[day][int(startTime[0])][minutes] = scheduleCount
+                
+        for hours in range(int(startTime[0]) + 1, int(endTime[0])):
+            for minutes in range(0,60):
+                scheduleArray[day][hours][minutes] = scheduleCount
+    scheduleCount += 1
+                
+currentTime = time.localtime(time.time())
+currentTiming = scheduleArray[currentTime.tm_wday][currentTime.tm_hour][currentTime.tm_min]
+root = tk.Tk()
+root.overrideredirect(True) # remove borders
+#print(f"{root.winfo_screenwidth()},{root.winfo_screenheight()}")
+
+x,y= 0,0
+monitors = sorted(get_monitors(), key=lambda m:m.x)
+for monitor in monitors:
+    x += monitor.width
+    y = max(monitor.height, y)
+    print(str(monitor))
+print(f"{x},{y}")
+root.geometry(f"{x}x{y}+0+0")
+root.configure(background='black')
+
+switchEvent = td.Event()
+tracker = SummaryTracker()
+
+def startTiming():
+    monitor = monitors[0]
+
+    if currentTiming != 0:
+        playlists = scheduleDict[currentTiming]
+        
+        for index in range(len(playlists)):
+            playlistName = playlists[index]
+            playlistFile = open(f"{directory}/{scheduleConfig['scheduleName']}/{playlistName}/{playlistName}_config.json")
+            playlistConfig = json.load(playlistFile)
+
+            if playlistConfig['aspectRatio'][1] == '8':
+                width = monitor.width
+                height = monitor.height//2
+
+                columnFrame = tk.Frame(root, width=monitor.width, height=monitor.height)
+                columnFrame.configure(background='black')
+                columnFrame.grid(row=0, column=index)
+                for row in range(len(playlistConfig['playlists'])):
+                    root.after(100,initPlaylist,playlistConfig['playlists'][row],playlistName,row,columnFrame,width,height)
+            else:
+                width = monitor.height * int(playlistConfig['aspectRatio'][0]) // int(playlistConfig['aspectRatio'][1])
+                height = monitor.height
+
+                columnFrame = tk.Frame(root, width=width, height=height)
+                columnFrame.configure(background='black')
+                columnFrame.grid(row=0, column=index)
+
+                root.after(100,initPlaylist,playlistConfig['playlists'][0],playlistName,0,columnFrame,width,height)
+    else:
+        root.after(100,waitSwitchThread,None,None)
+        
+def timingChecker():
+    global currentTiming
+    while True:
+        currentTime = time.localtime(time.time())
+        if (currentTiming != scheduleArray[currentTime.tm_wday][currentTime.tm_hour][currentTime.tm_min]):
+            switchEvent.set()
+            currentTiming = scheduleArray[currentTime.tm_wday][currentTime.tm_hour][currentTime.tm_min]
+        time.sleep(1)
+
+def memoryProfiler():
+    while True:
+        tracker.print_diff()
+        print(time.asctime(time.localtime(time.time())))
+        time.sleep(1800)
+
+def initPlaylist(playlist,playlistName,row,columnFrame,width,height):
+
+    # for if screensize is dynamic
+    """ if y - height > 0:
+        bufferHeight = y - height """
+    
+    # init mediaFrames
+
+    imageFrames = []
+    for media in playlist:
+        extension = media[0].split('.')[1]
+        if extension == 'jpeg' or extension == 'png' or extension == 'jpg':
+            tempFrame = tk.Frame(columnFrame, width=width, height=height)
+            tempFrame.grid(row=row)
+            image = Image.open(f"{directory}/{scheduleConfig['scheduleName']}/{playlistName}/" + media[0])
+            display = ImageTk.PhotoImage(image.resize((width,height)))
+            label = tk.Label(tempFrame,image=display,borderwidth=0,highlightthickness=0)
+            label.image = display
+            label.grid(row=0)
+            tempFrame.grid_remove()
+            imageFrames.append(tempFrame)
+        else:
+            imageFrames.append(0)
+    
+    # for if screensize is dynamic
+    """ paddingFrame = None
+    if (monitor.height != y):
+        paddingFrame = tk.Frame(columnFrame, width=width, height=bufferHeight)
+        paddingFrame.configure(background='black')
+        paddingFrame.grid(row=1) """
+    
+    root.after(0,playMedia,playlist,playlistName,columnFrame,imageFrames,0,None,width,height)
+    root.after(0,waitSwitchThread,columnFrame,imageFrames)
+
+def waitSwitchThread(columnFrame,imageFrames):
+    thread = td.Thread(target=waitSwitch,args=(columnFrame,imageFrames))
+    thread.start()
+    
+def waitSwitch(columnFrame,imageFrames):
+    switch_isSet = switchEvent.wait()
+    time.sleep(5)
+    if imageFrames != None:
+        for frames in imageFrames:
+            if frames != 0:
+                frames.destroy()
+        if columnFrame != None:
+            columnFrame.destroy()
+    root.after(0,switchEvent.clear)
+    root.after(100,startTiming)
+
+def waitVideo(playlist,playlistName,columnFrame,imageFrames,instance,player,mediaIndex,previousFrame,width,height):
+    time.sleep(0.5)
+    while player.is_playing() and not switchEvent.is_set():
+        time.sleep(0.1)
+    
+    player.get_media().release()
+    player.release()
+    instance.release()
+    previousFrame.destroy()
+    if not switchEvent.is_set():
+        root.after(0,playMedia,playlist,playlistName,columnFrame,imageFrames,mediaIndex,None,width,height)
+
+def waitVideoThread(playlist,playlistName,columnFrame,imageFrames,instance,player,mediaIndex,previousFrame,width,height):
+    thread = td.Thread(target=waitVideo,args=(playlist,playlistName,columnFrame,imageFrames,instance,player,mediaIndex,previousFrame,width,height))
+    thread.start()
+
+def waitImage(playlist,playlistName,columnFrame,imageFrames,mediaIndex,previousFrame,width,height,delay):
+    switch_isSet = switchEvent.wait(int(delay))
+    if not switch_isSet:
+        root.after(0,playMedia,playlist,playlistName,columnFrame,imageFrames,mediaIndex,previousFrame,width,height)
+    
+def waitImageThread(playlist,playlistName,columnFrame,imageFrames,mediaIndex,previousFrame,width,height,delay):
+    thread = td.Thread(target=waitImage,args=(playlist,playlistName,columnFrame,imageFrames,mediaIndex,previousFrame,width,height,delay))
+    thread.start()
+
+def playMedia(playlist,playlistName,columnFrame,imageFrames,mediaIndex,previousFrame,width,height):
+    if mediaIndex == len(playlist):
+        mediaIndex = 0
+
+    media = playlist[mediaIndex]
+    extension = media[0].split('.')[1]
+
+    if previousFrame != None:
+        previousFrame.grid_forget()
+
+    if extension == 'jpeg' or extension == 'jpg' or extension == 'png':
+        print('jpeg')
+        imageFrame = imageFrames[mediaIndex]
+        imageFrame.grid()
+        root.after(0,waitImageThread,playlist,playlistName,columnFrame,imageFrames,mediaIndex+1,imageFrame,width,height,media[1])
+    elif extension == 'mp4' or extension == 'mov':
+        print('mp4')
+        mediaFrame = tk.Frame(columnFrame, width=width, height=height)
+        mediaFrame.configure(background='black')
+        mediaFrame.grid(row=0)
+        mediaFrame.grid_remove()
+        if _isLinux:
+            instance = vlc.Instance('--no-xlib --aout=adummy')
+        else:
+            instance = vlc.Instance('--aout=adummy')
+
+        player = instance.media_player_new()
+        vlcMedia = instance.media_new(f"{directory}/{scheduleConfig['scheduleName']}/{playlistName}/" + media[0])
+        player.set_media(vlcMedia)
+        window = mediaFrame.winfo_id()
+        if _isLinux:
+            player.set_xwindow(window)
+        elif _isWindows:
+            player.set_hwnd(window)
+        player.video_set_aspect_ratio(f"{width}:{height}")
+
+        root.after(0,player.play)
+        root.after(300,mediaFrame.grid)
+        root.after(500,waitVideoThread,playlist,playlistName,columnFrame,imageFrames,instance,player,mediaIndex+1,mediaFrame,width,height)
+
+td.Thread(target=timingChecker).start()
+#td.Thread(target=memoryProfiler).start()
+root.after(0,startTiming)
+root.mainloop()
